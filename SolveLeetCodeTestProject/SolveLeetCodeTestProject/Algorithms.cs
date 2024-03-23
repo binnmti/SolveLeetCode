898ef134afa255eadcd197283ab4eb1e9e1a9262
Bin Matsui <binnmtiisprogrammer@gmail.com>
2024-03-23 21:53:24 +0900

    Slove O(n)

--- SolveLeetCodeTestProject/SolveLeetCodeTestProject/Algorithms.cs
+++ SolveLeetCodeTestProject/SolveLeetCodeTestProject/Algorithms.cs
@@ -4,27 +4,15 @@ internal static class Algorithms
 {
     internal static int[] TwoSum(int[] nums, int target)
     {
-        var sortNums = nums.Select((n, idx) => new { n, idx })
-                .ToDictionary(x => x.idx, x => x.n)
-                .OrderBy(x => x.Value);
-        var leftIndex = 0;
-        var rightIndex = nums.Length - 1;
-        var leftValue = sortNums.ElementAt(leftIndex).Value;
-        var rightValue = sortNums.ElementAt(rightIndex).Value;
-        while (leftValue + rightValue != target)
+        var map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
         {
-            if (leftValue + rightValue < target)
+            if (map.TryGetValue(target - nums[i], out var value))
             {
-                leftIndex++;
+                return [value, i];
             }
-            else if (leftValue + rightValue > target)
-            {
-                rightIndex--;
-            }
-            leftValue = sortNums.ElementAt(leftIndex).Value;
-            rightValue = sortNums.ElementAt(rightIndex).Value;
+            map[nums[i]] = i;
         }
-        return [sortNums.ElementAt(leftIndex).Key, sortNums.ElementAt(rightIndex).Key];
+        return [];
     }
-
 }
