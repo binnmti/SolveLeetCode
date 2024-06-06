01e88c943dd8a8a2399cbd35f17130deee08dca9
Bin Matsui <binnmtiisprogrammer@gmail.com>
2024-06-06 23:15:16 +0900

    Add 2405

--- SolveLeetCodeTestProject/SolveLeetCodeTestProject/AlgorithmsTest.cs
+++ SolveLeetCodeTestProject/SolveLeetCodeTestProject/AlgorithmsTest.cs
@@ -4,6 +4,27 @@ namespace SolveLeetCodeTestProject;
 public class AlgorithmsTest
 {
     [TestMethod]
+    [DataRow("abacaba", 4)]
+    [DataRow("ssssss", 6)]
+    public void AlgorithmsTest2405(string s, int expected)
+    {
+        var actual = Solution2405.PartitionString(s);
+        Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    [DataRow(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }, 6)]
+    [DataRow(new int[] { 1 }, 1)]
+    [DataRow(new int[] { 5, 4, -1, 7, 8 }, 23)]
+    [DataRow(new int[] { -2, -1 }, -1)]
+    public void AlgorithmsTest53(int[] nums, int expected)
+    {
+        var actual = Solution053.MaxSubArray(nums);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
     [DataRow("abcabcbb", 3)]
     public void AlgorithmsTest3(string s, int expected)
     {
---
+++ SolveLeetCodeTestProject/SolveLeetCodeTestProject/Solution2405.cs
@@ -0,0 +1,22 @@
+﻿
+namespace SolveLeetCodeTestProject
+{
+    internal class Solution2405
+    {
+        internal static int PartitionString(string s)
+        {
+            int co = 1;
+            var hits = new List<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (hits.Any(x => x == s[i]))
+                {
+                    co++;
+                    hits.Clear();
+                }
+                hits.Add(s[i]);
+            }
+            return co;
+        }
+    }
+}
