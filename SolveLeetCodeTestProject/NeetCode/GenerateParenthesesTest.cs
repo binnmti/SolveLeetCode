324f5cc19d1d781e116a464d9c50f687faf13291
binnmti <binnmtiisprogrammer@gmail.com>
2024-12-10 09:41:18 +0900

    2^2n * n Recursion

--- SolveLeetCodeTestProject/NeetCode/GenerateParenthesesTest.cs
+++ SolveLeetCodeTestProject/NeetCode/GenerateParenthesesTest.cs
@@ -10,40 +10,44 @@ internal class GenerateParenthesesTest
         Assert.AreEqual(ret, ["()"]);
     }
 
-    //Time complexity O(2^2n)
-    //Space complexity O(n)
-    List<string> GenerateParenthesis(int n)
+    private static bool Valid(string str)
+    {
+        int open = 0;
+        foreach (var s in str)
+        {
+            if (s == '(')
+            {
+                open++;
+            }
+            else
+            {
+                open--;
+            }
+            if (open < 0) return false;
+        }
+        return open == 0;
+    }
+
+    private static void Dfs(string str, List<string> list, int n)
+    {
+        if (str.Length == 2 * n)
+        {
+            if (Valid(str))
+            {
+                list.Add(str);
+            }
+            return;
+        }
+        Dfs(str + "(", list, n);
+        Dfs(str + ")", list, n);
+    }
+
+    // Time complexity O(2^2n * n)
+    // Space complexity O(2^2n * n)
+    public List<string> GenerateParenthesis(int n)
     {
         var list = new List<string>();
-        for (int i = 0; i < Math.Pow(2, 2 * n); i++)
-        {
-            bool isValid = true;
-            string parenth = "";
-            var stack = new Stack<string>();
-            for (int j = 0; j < 2 * n; j++)
-            {
-                bool open = (i & (int)Math.Pow(2, j)) == 0;
-
-                parenth += open ? "(" : ")";
-                if (open)
-                {
-                    stack.Push("(");
-                }
-                else
-                {
-                    if (stack.Count == 0)
-                    {
-                        isValid = false;
-                        break;
-                    }
-                    stack.Pop();
-                }
-            }
-            if (isValid && stack.Count == 0)
-            {
-                list.Add(parenth);
-            }
-        }
+        Dfs("", list, n);
         return list;
     }
 }
