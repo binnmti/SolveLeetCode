502e090c01aba8755a301fc6f197569418ee1ceb
binnmti <binnmtiisprogrammer@gmail.com>
2025-01-15 09:38:31 +0900

    Change Linq Algorithm

--- SolveLeetCodeTestProject/NeetCode/ValidPalindromeTest.cs
+++ SolveLeetCodeTestProject/NeetCode/ValidPalindromeTest.cs
@@ -14,30 +14,8 @@ public class ValidPalindromeTest
     //Space complexity O(n)
     public bool IsPalindrome(string s)
     {
-        s = s.ToUpper();
-        var s2 = "";
-        foreach (var c in s)
-        {
-            if ((c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
-            {
-                s2 += c.ToString();
-            }
-        }
-        var start = "";
-        for (int i = 0; i < s2.Length; i++)
-        {
-            var c = s2[i].ToString();
-            start += c;
-        }
-        var end = "";
-        for (int i = s2.Length - 1; i >= 0; i--)
-        {
-            var c = s2[i].ToString();
-            end += c;
-        }
-        Console.WriteLine(start);
-        Console.WriteLine(end);
-        return start == end;
+        var normalized = s.Where(char.IsLetterOrDigit).Select(char.ToLower);
+        return normalized.SequenceEqual(normalized.Reverse());
     }
 }
 
