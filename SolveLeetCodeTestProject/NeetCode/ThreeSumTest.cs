ce8cddae4c7e42cc497fd6d4ee3c4804d70737da
binnmti <binnmtiisprogrammer@gmail.com>
2025-01-17 08:49:15 +0900

    Add ThreeSum

---
+++ SolveLeetCodeTestProject/NeetCode/ThreeSumTest.cs
@@ -0,0 +1,19 @@
+﻿namespace SolveLeetCodeTestProject.NeetCode;
+
+[TestClass]
+public class ThreeSumTest
+{
+    [TestMethod]
+    public void ThreeSum()
+    {
+        ReferenceEquals(ThreeSum([-1, 0, 1, 2, -1, -4]), new List<List<int>>() { new() { -1, -1, 2 }, new() { -1, 0, 1 } });
+    }
+
+    //Time complexity O(n)
+    //Space complexity O(1)
+    public List<List<int>> ThreeSum(int[] nums)
+    {
+        return [];
+    }
+}
+
