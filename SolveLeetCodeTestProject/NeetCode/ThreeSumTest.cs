7384b64bcf9379ebba6c00fac6eaf62f38985a9e
binnmti <binnmtiisprogrammer@gmail.com>
2025-01-22 09:31:18 +0900

    ThreeSum O(n^2) Clear

--- SolveLeetCodeTestProject/NeetCode/ThreeSumTest.cs
+++ SolveLeetCodeTestProject/NeetCode/ThreeSumTest.cs
@@ -6,40 +6,46 @@ public class ThreeSumTest
     [TestMethod]
     public void ThreeSum()
     {
-        Assert.IsTrue(ThreeSum([-1, 0, 1, 2, -1, -4]).SequenceEqual(new List<List<int>>() { new() { -1, -1, 2 }, new() { -1, 0, 1 } }));
+        Array.ReferenceEquals(ThreeSum([-1, 0, 1, 2, -1, -4]), new List<List<int>>() { new() { 1, 1, 2 }, new() { -1, 0, 1 } });
         //ReferenceEquals(ThreeSum([0, 0, 0, 0]), new List<List<int>>() { new() { 0, 0, 0 } });
         //ReferenceEquals(ThreeSum([1, 1, 1]), new List<List<int>>());
         //ReferenceEquals(ThreeSum([-2, 0, 1, 1, 2]), new List<List<int>>() { new() { -2, 0, 2 }, new() { -2, 1, 1 } });
     }
 
-    //Time complexity O(n^3)
-    //Space complexity O(1)
+    //Time complexity O(n^2)
+    //Space complexity O(n)
     public List<List<int>> ThreeSum(int[] nums)
     {
         Array.Sort(nums);
 
-        var hashset = new HashSet<(int, int, int)>();
+        var listlist = new List<List<int>>();
         for (int i = 0; i < nums.Length; i++)
         {
-            for (int j = i + 1; j < nums.Length; j++)
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            var left = i + 1;
+            var right = nums.Length - 1;
+            while (left < right)
             {
-                for (int k = j + 1; k < nums.Length; k++)
+                var sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0)
                 {
-                    if (nums[i] + nums[j] + nums[k] == 0)
+                    var list = new List<int>
                     {
-                        hashset.Add((nums[i], nums[j], nums[k]));
+                        nums[i], nums[left], nums[right]
+                    };
+                    listlist.Add(list);
+
+                    left++;
+                    while (left < right && nums[left] == nums[left - 1])
+                    {
+                        left++;
                     }
                 }
+                else if (sum > 0) right--;
+                else left++;
             }
-        }
-        var listlist = new List<List<int>>();
-        foreach (var value in hashset)
-        {
-            var list = new List<int>
-            {
-                value.Item1, value.Item2, value.Item3
-            };
-            listlist.Add(list);
         }
         return listlist;
     }
