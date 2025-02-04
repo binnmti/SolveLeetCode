bbc6b0d6ddf73e13eca04e94159c9ea358a3a27e
Bin Matsui <binnmtiisprogrammer@gmail.com>
2025-02-04 14:35:05 +0900

    Refactor

--- SolveLeetCodeTestProject/NeetCode/LongestSubstringWithoutRepeatingCharactersTest.cs
+++ SolveLeetCodeTestProject/NeetCode/LongestSubstringWithoutRepeatingCharactersTest.cs
@@ -14,29 +14,24 @@ public class LongestSubstringWithoutRepeatingCharactersTest
     public int LengthOfLongestSubstring(string s)
     {
         int max = 0;
-        // // 1文字ずつふやしていき、つど同じ文字があるかチェック。
+        // 1文字ずつふやしていき、つど同じ文字があるかチェック。
         for (int i = 0; i < s.Length; i++)
         {
-            for (int j = 1; j < s.Length; j++)
+            for (int j = 1; j <= s.Length; j++)
             {
                 if (i + j > s.Length) break;
 
                 var str = s.Substring(i, j);
-
-                bool ng = false;
                 var hs = new HashSet<char>();
                 foreach (var c in str)
                 {
                     if (hs.Contains(c))
                     {
-                        ng = true;
+                        break;
                     }
                     hs.Add(c);
                 }
-                if (!ng)
-                {
-                    max = Math.Max(max, j);
-                }
+                max = Math.Max(max, j);
             }
         }
         return max;
