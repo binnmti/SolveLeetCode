ac303eca46e2aba9f8c801148e5f5ef32ee03c45
binnmti <binnmtiisprogrammer@gmail.com>
2025-02-17 09:42:50 +0900

    PermutationInString O(m * n)

--- SolveLeetCodeTestProject/NeetCode/PermutationInStringTest.cs
+++ SolveLeetCodeTestProject/NeetCode/PermutationInStringTest.cs
@@ -6,21 +6,27 @@ public class PermutationInStringTest
     [TestMethod]
     public void PermutationInString()
     {
+        Assert.AreEqual(CheckInclusion("adc", "dcda"), true);
         Assert.AreEqual(CheckInclusion("abc", "lecabee"), true);
     }
 
-    // Time complexity: O(n log n)
+    // Time complexity: O(n + m)
     // Space complexity: O(1)
     public bool CheckInclusion(string s1, string s2)
     {
-        var s1sort = string.Concat(s1.OrderBy(x => x).ToArray());
-        for (int i = 0; i < s2.Length - s1.Length + 1; i++)
+        var dic1 = new Dictionary<char, int>();
+        foreach (var c1 in s1)
         {
-            var s2sort = string.Concat(s2.Substring(i, s1.Length).OrderBy(x => x).ToArray());
-            if (s1sort == s2sort)
+            dic1[c1] = dic1.TryGetValue(c1, out int value) ? value + 1 : 1;
+        }
+        for (int i = 0; i <= s2.Length - s1.Length; i++)
+        {
+            var dic2 = new Dictionary<char, int>();
+            foreach (var c2 in s2.Substring(i, s1.Length))
             {
-                return true;
+                dic2[c2] = dic2.TryGetValue(c2, out int value) ? value + 1 : 1;
             }
+            if (!dic1.Any(x => !dic2.TryGetValue(x.Key, out int value) || x.Value != value)) return true;
         }
         return false;
     }
