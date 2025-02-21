b85df6425a58d5fd6888568c676eb19fc8e0a9f6
binnmti <binnmtiisprogrammer@gmail.com>
2025-02-21 09:59:50 +0900

    O(n^2) Refactor

--- SolveLeetCodeTestProject/NeetCode/MinimumWindowSubstringTest.cs
+++ SolveLeetCodeTestProject/NeetCode/MinimumWindowSubstringTest.cs
@@ -6,70 +6,35 @@ public class MinimumWindowSubstringTest
     [TestMethod]
     public void MinimumWindowSubstring()
     {
-        //Assert.AreEqual(MinWindow("abbbbbcdd", "abcdd"), "abbbbbcdd");
-        //Assert.AreEqual(MinWindow("ADOBECODEBANC", "ABC"), "BANC");
+        Assert.AreEqual(MinWindow("abbbbbcdd", "abcdd"), "abbbbbcdd");
+        Assert.AreEqual(MinWindow("ADOBECODEBANC", "ABC"), "BANC");
         Assert.AreEqual(MinWindow("OUZODYXAZV", "XYZ"), "YXAZ");
     }
 
-    // Time complexity: O(n^3)
+    // Time complexity: O(n^2)
     // Space complexity: O(1)
     public string MinWindow(string s, string t)
     {
         if (s.Length < t.Length) return "";
-
-        var tDic = new Dictionary<char, int>();
-        foreach (var c in t)
-        {
-            tDic[c] = tDic.ContainsKey(c) ? tDic[c] + 1 : 1;
-        }
+        var tDic = t.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
 
         // tは短い。sは長い。sから、tを含む塊を取得する。tのSlide Windowsは決まっていそう
         string min = "";
         int l = 0;
         for (int r = 0; r < s.Length; r++)
         {
-            var sDic = new Dictionary<char, int>();
-            foreach (var c in s[l..(r + 1)])
-            {
-                sDic[c] = sDic.ContainsKey(c) ? sDic[c] + 1 : 1;
-            }
-
-            // ここでlを進め条件チェック
-            while(l < r && (!tDic.ContainsKey(s[l]) || sDic[s[l]] > tDic[s[l]]))
+            var sDic = s[l..(r + 1)].GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            while (l < r && (!tDic.ContainsKey(s[l]) || sDic[s[l]] > tDic[s[l]]))
             {
                 l++;
             }
-
-            sDic.Clear();
-            foreach (var c in s[l..(r + 1)])
+            if (tDic.Any(x => !sDic.ContainsKey(x.Key) || sDic[x.Key] < x.Value))
             {
-                sDic[c] = sDic.ContainsKey(c) ? sDic[c] + 1 : 1;
+                continue;
             }
-
-            // ここで条件チェック
-            bool error = false;
-            foreach (var (k, v) in tDic)
+            if (min == "" || s[l..(r + 1)].Length < min.Length)
             {
-                if (!sDic.ContainsKey(k))
-                {
-                    error = true;
-                    break;
-                }
-                else
-                {
-                    if (sDic[k] < v)
-                    {
-                        error = true;
-                        break;
-                    }
-                }
-            }
-            if (!error)
-            {
-                if (min == "" || s[l..(r + 1)].Length < min.Length)
-                {
-                    min = s[l..(r + 1)];
-                }
+                min = s[l..(r + 1)];
             }
         }
         return min;
