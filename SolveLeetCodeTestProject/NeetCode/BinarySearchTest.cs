c7c8c4ccc92ee76c06dd9706558fa7339d2e272a
Bin Matsui <binnmtiisprogrammer@gmail.com>
2025-03-04 07:20:05 +0900

    BinarySearchTest O(n)

---
+++ SolveLeetCodeTestProject/NeetCode/BinarySearchTest.cs
@@ -0,0 +1,27 @@
+﻿namespace SolveLeetCodeTestProject.NeetCode;
+
+[TestClass]
+public class BinarySearchTest
+{
+    [TestMethod]
+    public void BinarySearch()
+    {
+        Assert.AreEqual(Search([-1, 0, 2, 4, 6, 8], 4), 3);
+        Assert.AreEqual(Search([-1, 0, 2, 4, 6, 8], 4), 3);
+    }
+
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    public int Search(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
