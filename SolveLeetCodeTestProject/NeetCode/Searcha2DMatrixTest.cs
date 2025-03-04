c639c007c1fb71d2d0617dcfea76e5ac836f5d63
Bin Matsui <binnmtiisprogrammer@gmail.com>
2025-03-05 06:25:31 +0900

    Searcha2DMatrix O(n^2)

---
+++ SolveLeetCodeTestProject/NeetCode/Searcha2DMatrixTest.cs
@@ -0,0 +1,30 @@
+﻿namespace SolveLeetCodeTestProject.NeetCode;
+
+[TestClass]
+public class Searcha2DMatrixTest
+{
+    [TestMethod]
+    public void Searcha2DMatrix()
+    {
+        Assert.AreEqual(SearchMatrix([[1, 2, 4, 8], [10, 11, 12, 13], [14, 20, 30, 40]], 10), true);
+        Assert.AreEqual(SearchMatrix([[1, 2, 4, 8], [10, 11, 12, 13], [14, 20, 30, 40]], 15), false);
+    }
+
+    // Time complexity: O(n^2)
+    // Space complexity: O(1)
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                if (target == matrix[i][j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
