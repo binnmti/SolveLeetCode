5d281ca2ecaf1b160cf8588539e72e097dd1cc40
binnmti <binnmtiisprogrammer@gmail.com>
2025-03-13 10:57:33 +0900

    Refactor

--- SolveLeetCodeTestProject/NeetCode/KokoEatingBananasTest.cs
+++ SolveLeetCodeTestProject/NeetCode/KokoEatingBananasTest.cs
@@ -6,6 +6,7 @@ public class KokoEatingBananasTest
     [TestMethod]
     public void KokoEatingBananas()
     {
+        Assert.AreEqual(MinEatingSpeed([1, 1, 1, 999999999], 10), 142857143);
         Assert.AreEqual(MinEatingSpeed([332484035, 524908576, 855865114, 632922376, 222257295, 690155293, 112677673, 679580077, 337406589, 290818316, 877337160, 901728858, 679284947, 688210097, 692137887, 718203285, 629455728, 941802184], 823855818), 14);
         Assert.AreEqual(MinEatingSpeed([25, 10, 23, 4], 4), 25);
         Assert.AreEqual(MinEatingSpeed([3, 6, 7, 11], 8), 4);
@@ -16,20 +17,18 @@ public class KokoEatingBananasTest
     // Space complexity: O(1)
     public int MinEatingSpeed(int[] piles, int h)
     {
-        for (int speed = 1; speed <= piles.Max(x => x); speed++)
+        for (var speed = 1; ; speed++)
         {
             long sum = 0;
-            foreach (var pile in piles)
+            foreach (int pile in piles)
             {
-                sum += (int)Math.Ceiling((double)pile / speed);
+                sum += (long)Math.Ceiling((double)pile / speed);
             }
             if (sum <= h)
             {
                 return speed;
             }
         }
-        return -1;
-
     }
 }
 
