172503d06d8a2c597b677490049d0602eeb85dab
binnmti <binnmtiisprogrammer@gmail.com>
2025-03-17 09:22:58 +0900

    FindMinimuminRotatedSortedArray O(n) Clear

---
+++ SolveLeetCodeTestProject/NeetCode/FindMinimuminRotatedSortedArrayTest.cs
@@ -0,0 +1,19 @@
+﻿namespace SolveLeetCodeTestProject.NeetCode;
+
+[TestClass]
+public class FindMinimuminRotatedSortedArrayTest
+{
+    [TestMethod]
+    public void FindMinimuminRotatedSortedArray()
+    {
+        Assert.AreEqual(FindMin([3, 4, 5, 6, 1, 2]), 1);
+    }
+
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    public int FindMin(int[] nums)
+    {
+        return nums.Min(x => x);
+    }
+}
+
