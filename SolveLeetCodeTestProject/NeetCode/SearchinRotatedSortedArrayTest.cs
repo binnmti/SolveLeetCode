d51045f7c18844021b32e6d73f3780513fa3eba6
binnmti <binnmtiisprogrammer@gmail.com>
2025-03-21 09:59:21 +0900

    SearchinRotatedSortedArray O(n) Clear

---
+++ SolveLeetCodeTestProject/NeetCode/SearchinRotatedSortedArrayTest.cs
@@ -0,0 +1,27 @@
+﻿namespace SolveLeetCodeTestProject.NeetCode;
+
+[TestClass]
+public class SearchinRotatedSortedArrayTest
+{
+    [TestMethod]
+    public void SearchinRotatedSortedArray()
+    {
+        Assert.AreEqual(Search([3, 4, 5, 6, 1, 2], 1), 4);
+    }
+
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    public int Search(int[] nums, int target)
+    {
+        int idx = -1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (target == nums[i])
+            {
+                idx = i;
+            }
+        }
+        return idx;
+    }
+}
+
