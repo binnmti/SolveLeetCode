ba5ab7e8d9e54356d455e4b5d60cc547529a27f5
binnmti <binnmtiisprogrammer@gmail.com>
2025-03-24 10:08:47 +0900

    SearchinRotatedSortedArray O(log n) clear

--- SolveLeetCodeTestProject/NeetCode/SearchinRotatedSortedArrayTest.cs
+++ SolveLeetCodeTestProject/NeetCode/SearchinRotatedSortedArrayTest.cs
@@ -6,22 +6,36 @@ public class SearchinRotatedSortedArrayTest
     [TestMethod]
     public void SearchinRotatedSortedArray()
     {
+        Assert.AreEqual(Search([1, 3, 5], 1), 0);
+        Assert.AreEqual(Search([1, 3], 1), 0);
+        Assert.AreEqual(Search([1], 1), 0);
+        Assert.AreEqual(Search([3, 5, 6, 0, 1, 2], 4), -1);
+        Assert.AreEqual(Search([4, 5, 6, 7, 0, 1, 2], 0), 4);
         Assert.AreEqual(Search([3, 4, 5, 6, 1, 2], 1), 4);
     }
 
-    // Time complexity: O(n)
+    // Time complexity: O(lon n)
     // Space complexity: O(1)
     public int Search(int[] nums, int target)
     {
-        int idx = -1;
-        for (int i = 0; i < nums.Length; i++)
+        int l = 0;
+        int r = nums.Length - 1;
+        while (l <= r)
         {
-            if (target == nums[i])
+            var m = l + (r - l) / 2;
+            if (nums[m] == target) return m;
+            if (nums[l] == target) return l;
+            if (nums[r] == target) return r;
+            if (nums[l] <= nums[m])
             {
-                idx = i;
+                l = m + 1;
+            }
+            else
+            {
+                r = m - 1;
             }
         }
-        return idx;
+        return -1;
     }
 }
 
