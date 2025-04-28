d47683b62f757d8f475cbb428a1522b21d650e2f
Bin Matsui <binnmtiisprogrammer@gmail.com>
2025-04-28 09:36:00 +0900

    CopyLinkedListwithRandomPointer Clear O(n)

---
+++ SolveLeetCodeTestProject/NeetCode/CopyLinkedListwithRandomPointerTest.cs
@@ -0,0 +1,38 @@
+﻿namespace SolveLeetCodeTestProject.NeetCode;
+
+[TestClass]
+public class CopyLinkedListwithRandomPointerTest
+{
+    [TestMethod]
+    public void CopyLinkedListwithRandomPointer()
+    {
+        var expected = copyRandomList(NodeExtensions.ToNode([[1, null], [2, 2], [3, 2]]));
+        //var actual = NodeExtensions.ToArray([1, 2, 4]);
+        //Assert.IsTrue(expected.Equal(actual));
+    }
+
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+    public Node copyRandomList(Node head)
+    {
+        var dic = new Dictionary<Node, Node>();
+        var current = head;
+        while (current != null)
+        {
+            var newNode = new Node(current.val);
+            dic[current] = newNode;
+            current = current.next;
+        }
+
+        current = head;
+        while (current != null)
+        {
+            var copy = dic[current];
+            copy.next = current.next != null ? dic[current.next] : null;
+            copy.random = current.random != null ? dic[current.random] : null;
+            current = current.next;
+        }
+        return head != null ? dic[head] : null;
+    }
+}
+
---
+++ SolveLeetCodeTestProject/Node.cs
@@ -0,0 +1,41 @@
+﻿namespace SolveLeetCodeTestProject;
+
+public class Node
+{
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int _val)
+    {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
+
+public static class NodeExtensions
+{
+    public static Node ToNode(this List<int?[]> ints)
+    {
+        var head = new Node(0);
+        var current = head;
+        foreach (var i in ints)
+        {
+            current.next = new Node(i.First().Value);
+            current = current.next;
+        }
+        return head.next ?? new Node(0);
+    }
+
+    public static int[] ToArray(this Node? node)
+    {
+        var list = new List<int>();
+        while (node != null)
+        {
+            list.Add(node.val);
+            node = node.next;
+        }
+        return [.. list];
+    }
+}
