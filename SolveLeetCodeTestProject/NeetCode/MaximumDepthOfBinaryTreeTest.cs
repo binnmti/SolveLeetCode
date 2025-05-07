23a2d7fded6486e5ffd0201cb64c363afbe345d8
Bin Matsui <binnmtiisprogrammer@gmail.com>
2025-05-07 09:39:47 +0900

    MaximumDepthOfBinaryTree Clear O(n)

---
+++ SolveLeetCodeTestProject/NeetCode/MaximumDepthOfBinaryTreeTest.cs
@@ -0,0 +1,23 @@
+﻿namespace SolveLeetCodeTestProject.NeetCode;
+
+[TestClass]
+public class MaximumDepthOfBinaryTreeTest
+{
+    [TestMethod]
+    public void MaximumDepthOfBinaryTree()
+    {
+        var expected = MaxDepth(TreeNodeExtensions.ToTreeNode([1, 2, 3, null, null, 4]));
+        //var actual = NodeExtensions.ToArray([1, 2, 4]);
+        //Assert.IsTrue(expected.Equal(actual));
+    }
+
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null) return 0;
+
+        return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+    }
+}
+
---
+++ SolveLeetCodeTestProject/TreeNode.cs
@@ -0,0 +1,36 @@
+﻿namespace SolveLeetCodeTestProject;
+
+
+public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+{
+    public int val = val;
+    public TreeNode? left = left;
+    public TreeNode? right = right;
+}
+
+
+public static class TreeNodeExtensions
+{
+    public static TreeNode ToTreeNode(this List<int?> ints)
+    {
+        var head = new TreeNode(0);
+        var current = head;
+        foreach (var i in ints)
+        {
+            current.left = new TreeNode(i.Value);
+            current = current.left;
+        }
+        return head.left ?? new TreeNode(0);
+    }
+
+    public static int[] ToArray(this TreeNode? node)
+    {
+        var list = new List<int>();
+        while (node != null)
+        {
+            list.Add(node.val);
+            node = node.left;
+        }
+        return [.. list];
+    }
+}
