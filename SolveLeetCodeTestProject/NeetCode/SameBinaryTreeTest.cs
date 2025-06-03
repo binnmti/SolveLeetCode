a889b09eb530378e47b8d9e98324a375ae68e8af
binnmti <binnmtiisprogrammer@gmail.com>
2025-06-03 13:05:27 +0900

    SameBinaryTree O(n) Clear

---
+++ SolveLeetCodeTestProject/NeetCode/SameBinaryTreeTest.cs
@@ -0,0 +1,28 @@
+﻿namespace SolveLeetCodeTestProject.NeetCode;
+
+[TestClass]
+public class SameBinaryTreeTest
+{
+    [TestMethod]
+    public void SameBinaryTree()
+    {
+        var expected = IsSameTree(TreeNodeExtensions.ToTreeNode([1, 2, 3]), TreeNodeExtensions.ToTreeNode([1, 2, 3]));
+        //var actual = NodeExtensions.ToArray([1, 2, 4]);
+        //Assert.IsTrue(expected.Equal(actual));
+    }
+
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+    public bool IsSameTree(TreeNode? p, TreeNode? q)
+    {
+        if (p != null && q != null)
+        {
+            if (!IsSameTree(p.left, q.left)) return false;
+            if (!IsSameTree(p.right, q.right)) return false;
+            return p.val == q.val;
+        }
+        return p == null && q == null;
+
+    }
+}
+
